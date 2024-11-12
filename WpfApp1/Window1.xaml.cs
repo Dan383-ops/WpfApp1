203992a0a4d2953570f68cc91e41555cb9890946
Половьянов Данил <Danil_Polovianov@stud.vvsu.ru>
2024-11-12 18:08:33 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+
+		private void btn7_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn8_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn9_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn4_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn5_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn6_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn1_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn2_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn3_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn0_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnDecimal_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnEquals_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnAdd_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnRemove_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnclear_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnMuti_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnDivis_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		/*private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow window = new MainWindow();
+			window.Show();
+			this.Close();
+		}
+		private void MenuItem1_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			TextBox textBox = sender as TextBox;
+			MessageBox.Show(textBox.Text);
+		}
+
+		private void btn4_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	*/
+	}
+}
